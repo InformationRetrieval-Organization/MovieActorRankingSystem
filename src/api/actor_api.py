0d3995305cc0c3a125307edb454d8b4760a5c1a8
Tony <tonymeissner70@gmail.com>
2024-06-01 11:42:11 +0900

    resolves build API endpoint for returning actors #8

--- src/api/actor_api.py
+++ src/api/actor_api.py
@@ -1,6 +1,10 @@
 from fastapi import APIRouter
+from prisma import models
+from typing import List
+from db.actor import get_all_actors
 
 router = APIRouter()
+
 
 @router.post(
     "/search/actor",
@@ -8,11 +12,15 @@ router = APIRouter()
         429: {"description": "Too Many Requests"},
     },
 )
-async def search_actor(q: str) -> None:
+async def search_actor(q: str) -> List[models.Actor]:
     """
     Search the Boolean Model for the given query.<br>
     Example usage: http://127.0.0.1:8000/search/boolean
     """
     query = q
+    print(f"Query: {query}")
 
-    return None
+    # TODO: Implement search functionality
+    actors = await get_all_actors()
+
+    return actors
