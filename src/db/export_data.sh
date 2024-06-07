0ce2d8dca4f6e29897dae9e5971afc40b4b740f1
Tony <tonymeissner70@gmail.com>
2024-06-07 16:54:35 +0900

    new dump

--- src/db/export_data.sh
+++ src/db/export_data.sh
@@ -7,7 +7,7 @@ USERNAME=postgres
 OUTPUT_FILE=database_dump.sql
 
 # Export the data
-docker exec -t $CONTAINER_NAME pg_dump -U $USERNAME -d $DATABASE_NAME -F c -b -v --data-only -f  /tmp/$OUTPUT_FILE
+docker exec -t $CONTAINER_NAME pg_dump -U $USERNAME -d $DATABASE_NAME -F c -b -v -f  /tmp/$OUTPUT_FILE
 
 # Copy the dump file to the local machine
 docker cp $CONTAINER_NAME:/tmp/$OUTPUT_FILE .
