325b028161551f551dda4f5ed7c74c3f3d67efc7
Tony <tonymeissner70@gmail.com>
2024-06-09 13:42:46 +0900

    extracted classification to extra file

---
+++ src/utils/classification.py
@@ -0,0 +1,30 @@
+from typing import List
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, pipeline
+import globals
+
+
+def load_classification_model() -> pipeline:
+    """
+    Load the classification model and tokenizer from the Hugging Face model hub
+    """
+    model_name = "zbnsl/bert-base-uncased-emotionsModified"
+
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModelForSequenceClassification.from_pretrained(model_name)
+
+    # Initialize the pipeline with the local model and tokenizer
+    globals._classifier = pipeline(
+        task="text-classification", model=model, tokenizer=tokenizer, top_k=None
+    )
+    return globals._classifier
+
+
+def get_classification(text: List[str]):
+    """
+    Perform text classification using the loaded model
+    """
+    if globals._classifier is None:
+        globals._classifier = load_classification_model()
+
+    # Perform classification
+    return globals._classifier(text)
