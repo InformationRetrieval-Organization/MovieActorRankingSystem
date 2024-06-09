4d484af42a095f91a69bff70afc19e22e7bbfd1d
Patrik <patriklytschmann@web.de>
2024-06-09 14:39:41 +0900

    implementation of build classified vector space

--- src/information_retrieval/classified_vector_space_model.py
+++ src/information_retrieval/classified_vector_space_model.py
@@ -7,9 +7,8 @@ from db.role import get_all_roles
 import globals
 from tqdm import tqdm
 from concurrent.futures import ProcessPoolExecutor
-import os
 from prisma import models
-from db.actor_classifier import get_all_classified_actors
+from db.actor_classifier import get_all_actor_classifiers
 
 
 async def search_classified_vector_space_model(query: List[str]) -> List[int]:
@@ -33,5 +32,24 @@ async def build_classified_vector_space_model():
     Calculate the vectors for every actor based on their classification
     """
     # Get all classified actors
-    classified_actors = await get_all_classified_actors()
+    classified_actors = await get_all_actor_classifiers()
     # Caculate vectors for each actor
+    for actor in classified_actors:
+        # Calculate the vector for the actor
+        vector = calculate_actor_vector(actor)
+        globals._classified_actors_vector_map[actor.id] = vector
+
+    print(globals._classified_actors_vector_map)
+
+
+def calculate_actor_vector(actor: models.ActorClassifier) -> List[float]:
+    """
+    Read the values for the classification and calculate the vector for the actor
+    """
+    vector = []
+    vector.extend(actor.angerScore)
+    vector.extend(actor.fearScore)
+    vector.extend(actor.joyScore)
+    vector.extend(actor.sadnessScore)
+    vector.extend(actor.loveScore)
+    vector.extend(actor.surpriseScore)
